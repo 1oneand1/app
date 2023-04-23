18131e7cd089089233a5d5450e5e90e814fc3429
1oneand1 <123364703+1oneand1@users.noreply.github.com>
2023-04-23 16:48:23 +0100

    Rubik Cube

---
+++ rubik_cube.py
@@ -0,0 +1,7 @@
+import random    # the chance of the game
+random_number = random.randint(18, 26)   # max and min moves to solve
+sides_of_colors = 6 + 6 * 3   # the total moves needed in worst case
+time_to_1st = sides_of_colors - random_number 
+total_moves = sides_of_colors - time_to_1st  # total moves for this cube 
+print(f"Total moves of player to solve: {total_moves}")   
+    
